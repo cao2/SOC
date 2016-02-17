ea522664f330e5444503e1fdd662ac45732c7127
cao2 <cao2@mail.usf.edu>
2016-02-17 17:13:24 -0500

    sdfs
    asdfads

--- SOC.srcs/sources_1/new/top.vhd
+++ SOC.srcs/sources_1/new/top.vhd
@@ -49,6 +49,7 @@ architecture Behavioral of top is
    signal snoop_hit1, snoop_hit2: boolean;
    signal snoop_res1, snoop_res2, bus_req1, bus_req2: std_logic_vector(50 downto 0);
    signal  memres, tomem : std_logic_vector(51 downto 0);
+   signal full_crq1, full_srq1, full_brs1,full_wb1,full_srs1,full_crq2, full_srq2, full_brs2,full_wb2,full_srs2:std_logic;
 begin
     clk_gen : process
        
@@ -72,9 +73,9 @@ begin
        full_c=>full_c1_u
    );
    
-   cpu2: entity xil_defaultlib.CPU(Behavioral) port map(
+   cpu2: entity xil_defaultlib.CPU2(Behavioral) port map(
           Clock=>Clock,
-          seed=>10,
+          seed=>5,
           cpu_res=>cpu_res2,
           cpu_req=>cpu_req2,
           full_c=>full_c2_u
@@ -88,11 +89,14 @@ begin
          snoop_hit=>snoop_hit1,
          snoop_res=>snoop_res1,
          bus_req=>bus_req1,
-         full_c_u=>full_c1_u,
-         full_c_b=>full_c1_b,
-         full_b_c=>full_b_c1
+         full_srq=>full_srq1,
+         full_brs=>full_brs1,
+         full_crq=>full_crq1,
+         full_wb=>full_wb1,
+         full_srs=>full_srs1
+          
     );
-     cache2: entity xil_defaultlib.L1Cache(Behavioral) port map(
+     cache2: entity xil_defaultlib.L1Cache2(Behavioral) port map(
             Clock=>Clock,
             req=>cpu_req2,
             snoop_req=>snoop_req2,
@@ -101,9 +105,11 @@ begin
             snoop_hit=>snoop_hit2,
             snoop_res=>snoop_res2,
             bus_req=>bus_req2,
-            full_c_u=>full_c1_u,
-            full_c_b=>full_c2_b,
-            full_b_c=>full_b_c2
+            full_srq=>full_srq2,
+         	full_brs=>full_brs2,
+         	full_crq=>full_crq2,
+         	full_wb=>full_wb2,fd
+         	full_srs=>full_srs2
        );
     interconnect: entity xil_defaultlib.AXI(Behavioral) port map(
         Clock=>Clock,
